1783a34e46c836205a2639ebdd85cec5843d717f
nitin <nitin@incubyte.co>
2022-11-12 18:45:37 +0530

    ADD account duplication test

--- spec/graphql/mutations/create_user_spec.rb
+++ spec/graphql/mutations/create_user_spec.rb
@@ -1,28 +1,38 @@
 require "rails_helper"
 module Mutations
     RSpec.describe 'CreateUser', type: :request do
-      it "creates new user" do
-        post '/graphql', params: { query: query }
-        puts response.body
-        # result = B451Schema.execute(qs)
-        # puts result["data"].dig("testField")
+      it "creates new user" do			
+        post '/graphql', params: { query: query, variables: {
+					email: "cultsharing@email.com",
+					password: "12345678"} }
+				jsonResponse = JSON.parse(response.body)
 
-        expect(response.body.success).to eq(true)
-
+	    	expect(jsonResponse.dig("data", "createUser", "user", "id")).to eq("1")
       end
-    
-    	def query
-    	    <<-GRAPHQL
-					mutation type {
-						createUser(input: {authProvider: {credentials: {email: "email@example.com", password: "123456"}}}) {
-							user {
-								id
-								email
-							}
-							success
-						}
-					}					
-    	    GRAPHQL
-    	end
-    end
+			it "doesn't allow to create user with existing email" do			
+				User.create!(email: "cultsharing@email.com",
+					password: "12345678")
+
+        post '/graphql', params: { query: query, variables: {
+					email: "cultsharing@email.com",
+					password: "12345678"} }
+				jsonResponse = JSON.parse(response.body)
+
+	    	expect(jsonResponse["errors"].first["message"]).to eq("Account already exists with this email.".to_s)
+      end
+
+
+  		def query
+  		  <<-GRAPHQL
+				mutation CreateUser($email: String!, $password: String!){
+					createUser(input: {authProvider: {credentials: {email: $email, password: $password}}}) {
+						user {
+							id
+							email
+						
+					}
+				}					
+  		  GRAPHQL
+  		end
+  	end
 end
