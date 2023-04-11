8f0d817478de62020d70063fa28b39276085cf2c
nitin <nitin@incubyte.co>
2023-04-11 11:11:36 +0530

    Revert "first commit"
    This reverts commit 557b65eaa8b3e48ef962d7e45b6362c71af0b6c9.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,28 @@ class User < ApplicationRecord
     has_secure_password
     validates :email,  presence: true
     validates :password_digest,  presence: true
-    has_many  :posts		
+    has_many  :books
+
+    has_many :friend_sent, class_name: 'Friendship',
+                         foreign_key: 'sent_by_id',
+                         inverse_of: 'sent_by',
+                         dependent: :destroy
+    has_many :friend_request, class_name: 'Friendship',
+                         foreign_key: 'sent_to_id',
+                         inverse_of: 'sent_to',
+                         dependent: :destroy
+    has_many :friends, -> { merge(Friendship.friends) },
+            through: :friend_sent, source: :sent_to
+    has_many :pending_requests, -> { merge(Friendship.not_friends) },
+            through: :friend_sent, source: :sent_to
+    has_many :received_requests, -> { merge(Friendship.not_friends) },
+            through: :friend_request, source: :sent_by
+    has_many :book_sent, class_name: 'BookRequest',
+                         foreign_key: 'sent_by_id',
+                         inverse_of: 'sent_by',
+                         dependent: :destroy
+    has_many :book_request, class_name: 'BookRequest',
+                         foreign_key: 'sent_to_id',
+                         inverse_of: 'sent_to',
+                         dependent: :destroy		
 end
--- spec/factories/post.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    title{ Faker::Lorem.word }
-    subtitle{ Faker::Lorem.word }
-    excerpt{ Faker::Lorem.word }
-    content{ Faker::Lorem.word }
-  end
-end
